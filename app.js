6286d6108a47f876f1d642c9d41389b73acf796f
excf28 <deee4444@gmail.com>
2024-10-17 16:57:19 +0200

    dijkstra

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ ctx.strokeStyle = "white";
 const centerX = canvas.width / 2;
 const centerY = canvas.height / 2;
 
-const minX = 16.55;
-const minY = 47.125;
-const size = 0.2;
+const minX = 16.61;
+const minY = 47.18;
+const size = 0.1;
 
 const aspectRatio = canvas.width / canvas.height;
 
@@ -26,18 +26,115 @@ const mapX = (x) => {
 }
 
 const mapY = (y) => {
-    return ((y - minY) / (maxY - minY)) * canvas.height;
+    return canvas.height - (((y - minY) / (maxY - minY)) * canvas.height);
 }
 
-fetch('./nodes.json').then((response) => response.json())
-    .then((nodes) => {
-        for (const node of nodes) {
-            for (const connection of node.connections) {
-                ctx.beginPath();
-                ctx.moveTo(mapX(node.x), mapY(node.y));
-                const connectedNode = nodes[connection];
-                ctx.lineTo(mapX(connectedNode.x), mapY(connectedNode.y));
-                ctx.stroke();
-            }
+const invX = (x) => {
+    return ((x - (centerX - (size * canvas.width) / 2)) / canvas.width) * (maxX - minX) + minX;
+}
+
+const invY = (y) => {
+    return ((canvas.height - y) / canvas.height) * (maxY - minY) + minY;
+}
+
+const closestNode = (nodes, x, y) => {
+    let closest = nodes[0];
+    let closestDistance = (x - closest.x) ** 2 + (y - closest.y) ** 2
+    for (const node of nodes) {
+        if ((x - node.x) ** 2 + (y - node.y) ** 2 < closestDistance) {
+            closest = node;
+            closestDistance = (x - closest.x) ** 2 + (y - closest.y) ** 2
         }
-    });
+    }
+
+    return closest
+}
+
+const dijkstra = (nodes, start, end) => {
+    const visited = new Set();
+    const queue = [[start, 0]];
+    const previous = new Map();
+
+    let finished = false;
+
+    while (!finished) {
+        queue.sort((a, b) => a[1] - b[1]);
+        const [node, distance] = queue.shift();
+        for (const connection of nodes[node].connections) {
+            if (!visited.has(connection)) {
+                visited.add(connection);
+                const newDistance = (nodes[node].x - nodes[connection].x) ** 2 + (nodes[node].y - nodes[connection].y) ** 2
+                queue.push([connection, distance + newDistance]);
+                previous.set(connection, node);
+            }
+            if (connection === end) {
+                finished = true;
+                break;
+            };
+        }
+
+        const prev = previous.get(node);
+        if (prev !== undefined) {
+            ctx.strokeStyle = 'blue';
+            ctx.beginPath();
+            ctx.moveTo(mapX(nodes[node].x), mapY(nodes[node].y));
+            ctx.lineTo(mapX(nodes[prev].x), mapY(nodes[prev].y));
+            ctx.stroke();
+        }
+    }
+
+    let current = end;
+    while (current !== start) {
+        ctx.strokeStyle = 'lime';
+        ctx.beginPath();
+        ctx.moveTo(mapX(nodes[current].x), mapY(nodes[current].y));
+        ctx.lineTo(mapX(nodes[previous.get(current)].x), mapY(nodes[previous.get(current)].y));
+        ctx.stroke();
+        current = previous.get(current);
+    }
+}
+
+(async () => {
+    const nodes = await (await fetch('./nodes.json')).json();
+
+    for (const node of nodes) {
+        for (const connection of node.connections) {
+            ctx.beginPath();
+            ctx.moveTo(mapX(node.x), mapY(node.y));
+            const connectedNode = nodes[connection];
+            ctx.lineTo(mapX(connectedNode.x), mapY(connectedNode.y));
+            ctx.stroke();
+        }
+    }
+
+    let start = undefined;
+    let end = undefined;
+
+    canvas.addEventListener('click', (e) => {
+        const x = e.clientX - canvas.offsetLeft;
+        const y = e.clientY - canvas.offsetTop;
+
+        const closest = closestNode(nodes, invX(x), invY(y))
+
+        if (start === undefined || end) {
+            start = closest;
+            end = undefined;
+            ctx.strokeStyle = 'lime';
+            ctx.beginPath()
+            ctx.arc(mapX(closest.x), mapY(closest.y), 5, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+        else if (end === undefined) {
+            end = closest;
+            ctx.strokeStyle = 'red';
+            ctx.beginPath()
+            ctx.arc(mapX(closest.x), mapY(closest.y), 5, 0, 2 * Math.PI);
+            ctx.stroke();
+            dijkstra(nodes, nodes.indexOf(start), nodes.indexOf(end));
+        }
+    })
+
+})()
+
+
+
--- parse.js
+++ parse.js
@@ -3,7 +3,10 @@ const { writeFile } = require('fs');
 
 const nodes = [];
 
+const types = new Set();
+
 for (const feature of mapData.features) {
+    types.add(feature.geometry.type);
     if (feature.geometry.type === 'LineString' || feature.geometry.type === 'Polygon') {
         let coordinates = feature.geometry.type === 'LineString' ? feature.geometry.coordinates : feature.geometry.coordinates[0];
 
@@ -11,26 +14,31 @@ for (const feature of mapData.features) {
         for (const pos of coordinates) {
             const nodeIndex = nodes.findIndex(n => n.x === pos[0] && n.y === pos[1]);
             if (nodeIndex === -1) {
-                nodes.push({ x: pos[0], y: pos[1], connections: previousNode !== -1 ? [previousNode] : [], required: false });
+                nodes.push({ x: pos[0], y: pos[1], connections: previousNode !== -1 ? [previousNode] : []});
+                if (previousNode !== -1) nodes[previousNode].connections.push(nodes.length - 1);
             } else {
-                if (previousNode !== -1 && !nodes[nodeIndex].connections.includes(previousNode)) nodes[nodeIndex].connections.push(previousNode);
-                nodes[nodeIndex].required = true;
+                if (previousNode !== -1) {
+                    if (!nodes[nodeIndex].connections.includes(previousNode)) nodes[nodeIndex].connections.push(previousNode);
+                    if (!nodes[previousNode].connections.includes(nodeIndex)) nodes[previousNode].connections.push(nodeIndex);
+                };
             }
             previousNode = nodeIndex !== -1 ? nodeIndex : nodes.length - 1;
         }
     }
 }
 
-minX = nodes[0].x;
-maxX = nodes[0].x;
-minY = nodes[0].y;
-maxY = nodes[0].y;
-for (const node of nodes) {
-    if (node.x < minX) minX = node.x;
-    if (node.x > maxX) maxX = node.x;
-    if (node.y < minY) minY = node.y;
-    if (node.y > maxY) maxY = node.y;
-}
-console.log(minX, maxX, minY, maxY);
+console.log(types);
+
+// minX = nodes[0].x;
+// maxX = nodes[0].x;
+// minY = nodes[0].y;
+// maxY = nodes[0].y;
+// for (const node of nodes) {
+//     if (node.x < minX) minX = node.x;
+//     if (node.x > maxX) maxX = node.x;
+//     if (node.y < minY) minY = node.y;
+//     if (node.y > maxY) maxY = node.y;
+// }
+// console.log(minX, maxX, minY, maxY);
 
 writeFile('nodes.json', JSON.stringify(nodes), (e)=>{console.log(e)});
